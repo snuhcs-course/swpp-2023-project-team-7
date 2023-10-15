0e521d8da0978378542b00deaff6813a173927a2
Ikjun Choi <1350adwx@snu.ac.kr>
2023-10-15 22:45:00 +0900

    feat: Implement class for storing and finding summaries

--- backend/llama/run_summary.py
+++ backend/llama/run_summary.py
@@ -1,5 +1,8 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, TextStreamer
 import torch
+import sys
+import pickle
+from custom_type import Summary
 
 model_name_or_path = "TheBloke/Llama-2-7b-Chat-GPTQ"
 model = AutoModelForCausalLM.from_pretrained(model_name_or_path,
@@ -27,17 +30,22 @@ PROMPT_TEMPLATE_TOKENS_INTERMEDIATE = tokenizer(PROMPT_TEMPLATE_INTERMEDIATE, re
 PROMPT_TEMPLATE_TOKENS_FINAL = tokenizer(PROMPT_TEMPLATE_FINAL, return_tensors='pt').input_ids
 
 def split_large_text(story):
-	tokens = tokenizer(story, return_tensors='pt').input_ids
+	res = tokenizer(story, add_special_tokens=False, return_tensors='pt')
+	tokens = res.input_ids
 	sliced_tensors = []	
 	for start_idx in range(0, tokens.shape[1], MAX_SIZE):
-		sliced_tensors.append(tokens[:, start_idx:start_idx+MAX_SIZE])
+		end_idx = min(start_idx+MAX_SIZE, tokens.shape[1])
+		start = res.encodings[0].offsets[start_idx][0]
+		end = res.encodings[0].offsets[end_idx-1][1]
+
+		sliced_story = {"tokens":tokens[:, start_idx:start_idx+MAX_SIZE], "start_idx":start, "end_idx":end}
+		sliced_tensors.append(sliced_story)
 	return sliced_tensors
 
 def split_list(input_list):
 	if not input_list:
 		return []
-	
-	split_size = 4
+	split_size = 2
 	
 	# split the input_list into groups
 	num_groups = len(input_list) // split_size
@@ -59,7 +67,6 @@ def split_list(input_list):
 					remainder -= 1
 				else:
 					break
-
 	# create output_list
 	for i in range(num_groups):
 		output_list.append(input_list[:output_sizes[i]])
@@ -68,36 +75,54 @@ def split_list(input_list):
 	return output_list
 
 def reduce_multiple_summaries_to_one(summary_list, is_intermediate):
-	concat_summaries = '\n'.join(summary_list)
+	summary_content_list = [summary.summary_content for summary in summary_list]
+	reduced_start_idx = min([summary.start_idx for summary in summary_list])
+	reduced_end_idx = max([summary.end_idx for summary in summary_list])
+
+	concat_summaries = '\n'.join(summary_content_list)
 	prompt = tokenizer(concat_summaries, return_tensors='pt').input_ids
 
 	if is_intermediate:
 		inserted_input_ids = torch.cat([PROMPT_TEMPLATE_TOKENS_INTERMEDIATE[:,:-4], prompt, PROMPT_TEMPLATE_TOKENS_INTERMEDIATE[:,-4:]], dim=1).cuda()
 	else:
 		inserted_input_ids = torch.cat([PROMPT_TEMPLATE_TOKENS_FINAL[:,:-4], prompt, PROMPT_TEMPLATE_TOKENS_FINAL[:,-4:]], dim=1).cuda()
+
 	output = model.generate(inputs=inserted_input_ids, streamer=streamer, temperature=0.7, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=512)
-	reduced_summary = tokenizer.decode(output[0]).split("[/INST]")[1].replace("<s>", "").replace("</s>", "")
+	decoded_output = tokenizer.decode(output[0]).split("[/INST]")[1].replace("<s>", "").replace("</s>", "")
+
+	reduced_summary = Summary(summary_content=decoded_output, start_idx=reduced_start_idx, end_idx=reduced_end_idx, children=summary_list)
+	for summary in summary_list:
+		summary.parent = reduced_summary	
+
 	return reduced_summary
 
 def reduce_summaries_list(summaries_list):
 	while len(summaries_list) > 1:
 		double_paired_list = split_list(summaries_list)
-		summaries_list = [reduce_multiple_summaries_to_one(double_pair, is_intermediate=(len(summaries_list)>7)) for double_pair in double_paired_list]	
-	return summaries_list[0]	
+		summaries_list = [reduce_multiple_summaries_to_one(double_pair, is_intermediate=(len(summaries_list)>3)) for double_pair in double_paired_list]	
+	return summaries_list[0]
 		
 def main():
-	story = open("the_open_boat.txt", "r").read()
+	story_path = sys.argv[1]
+	story = open(story_path, "r").read()
 	summaries_list = []
 
 	print("\n\n*** Generate:")
-	sliced_tokens = split_large_text(story)
-	for prompt in sliced_tokens:
-		inserted_input_ids = torch.cat([PROMPT_TEMPLATE_TOKENS_INTERMEDIATE[:,:-4], prompt, PROMPT_TEMPLATE_TOKENS_INTERMEDIATE[:,-4:]], dim=1).cuda()
+	sliced_tokens_dict_list = split_large_text(story)
+	for prompt in sliced_tokens_dict_list:
+		inserted_input_ids = torch.cat([PROMPT_TEMPLATE_TOKENS_INTERMEDIATE[:,:-4], prompt["tokens"], PROMPT_TEMPLATE_TOKENS_INTERMEDIATE[:,-4:]], dim=1).cuda()
 		output = model.generate(inputs=inserted_input_ids, streamer=streamer, temperature=0.7, do_sample=True, top_p=0.95, top_k=40, max_new_tokens=512)
-		summaries_list.append(tokenizer.decode(output[0]).split("[/INST]")[1].replace("<s>", "").replace("</s>", ""))
-	
+		decoded_output = tokenizer.decode(output[0]).split("[/INST]")[1].replace("<s>", "").replace("</s>", "")
+
+		first_level_summary = Summary(summary_content=decoded_output,
+						start_idx=prompt["start_idx"],
+						end_idx=prompt["end_idx"])
+		summaries_list.append(first_level_summary)
+
 	single_summary = reduce_summaries_list(summaries_list)
-	print(single_summary)
+	print(single_summary.summary_content)
+	with open(f"{story_path.split('.')[0]}_summary.pkl", 'wb') as pickle_file:
+		pickle.dump(single_summary, pickle_file)
 
 if __name__ == "__main__":
 	main()
