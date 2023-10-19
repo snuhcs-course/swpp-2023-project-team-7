b92fc521181e4e15f758b4e7d1fe93f5e88bc142
chaemin2001 <chaemin2001@snu.ac.kr>
2023-10-19 20:26:42 +0900

    Add Image to Bottom View

--- frontend/app/src/main/java/com/example/shareader/BottomSheetView.kt
+++ frontend/app/src/main/java/com/example/shareader/BottomSheetView.kt
@@ -1,10 +1,15 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
@@ -12,9 +17,12 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.shareader.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,7 +53,20 @@ fun BookInfo(){
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp)
     ){
-        Text(text = "Alice's Adventures in Wonderland", fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        Card(
+            modifier = Modifier
+                .width(90.dp)
+                .height(120.dp)
+        ) {
+            Image(
+                painterResource(R.drawable.book_image),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        Text(text = "Alice's Adventures in Wonderland", fontWeight = FontWeight.Bold, fontSize = 20.sp,
+            modifier = Modifier.padding(top = 15.dp))
         Text(text = "Lewis Caroll")
     }
 }
