eefbedf448dba49829a3000ed6f3e0ba9faddd69
chaemin2001 <chaemin2001@snu.ac.kr>
2023-10-19 20:05:28 +0900

    BookList UI

--- frontend/app/src/main/java/com/example/shareader/BookListView.kt
+++ frontend/app/src/main/java/com/example/shareader/BookListView.kt
@@ -1,12 +1,25 @@
 package com.example.shareader
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.CheckCircle
 import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.sharp.MoreVert
@@ -26,92 +39,138 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun BookListView() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold (
+        floatingActionButton = { AddButton() }
+    ){
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
         ) {
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
+            Column(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
 
-
-                Text("My Library")
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Sharp.Settings,
-                        contentDescription = "setting",
-                        modifier = Modifier
-                            .width(25.dp)
-                            .height(25.dp)
-                    )
+                ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Spacer(modifier = Modifier.width(150.dp))
+                    Text("My Library", fontSize = 15.sp)
+                    Spacer(modifier = Modifier.width(120.dp))
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Outlined.Settings,
+                            contentDescription = "setting",
+                            modifier = Modifier
+                                .width(25.dp)
+                                .height(25.dp)
+                        )
+                    }
                 }
+                Divider(color = Color.Gray, thickness = 1.dp)
+                BookCard()
+                BookCard()
+                BookCard()
             }
-            BookCard()
         }
     }
 }
 
 @Composable
-fun BookCard() {
-    Row (
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ){
-        // image
-        Column {
-            Text("Alice's Adventures in Wonderland")
-            Text("Lewis Carroll")
-            Text("32%")
-        }
-        Column {
-            var showSheet by remember { mutableStateOf(false) }
-
-            if (showSheet) {
-                BottomSheet() {
-                    showSheet = false
-                }
-            }
-
-            IconButton(onClick = { }) {
-                Icon(
-                    imageVector = Icons.Rounded.CheckCircle,
-                    contentDescription = "uploaded",
-                    modifier = Modifier
-                        .width(20.dp)
-                        .height(20.dp)
-                )
-            }
-            IconButton(onClick = { showSheet = true }) {
-                Icon(
-                    imageVector = Icons.Sharp.MoreVert,
-                    contentDescription = "more",
-                    modifier = Modifier
-                        .width(20.dp)
-                        .height(20.dp)
-                )
-            }
-        }
+fun AddButton() {
+    FloatingActionButton(
+        onClick = {  },
+    ) {
+        Icon(Icons.Filled.Add, "Floating action button.")
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BottomSheet(onDismiss: () -> Unit) {
-    val modalBottomSheetState = rememberModalBottomSheetState()
-
-    ModalBottomSheet(
-        onDismissRequest = { onDismiss() },
-        sheetState = modalBottomSheetState,
-        dragHandle = { BottomSheetDefaults.DragHandle() },
-    ) {
-        Text(text = "test")
+fun BookCard() {
+    val context = LocalContext.current
+    val activityLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { _ ->
     }
+    Card(
+        onClick = { val intent = Intent(context, ReaderActivity::class.java)
+            activityLauncher.launch(intent) },
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Transparent
+        ),
+    ) {
+        Row (
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.width(10.dp))
+            Card(
+                modifier = Modifier
+                    .width(69.dp)
+                    .height(92.dp)
+            ) {
+                Image(
+                    painterResource(R.drawable.book_image),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            Column(
+                modifier = Modifier.padding(15.dp),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text("Alice's Adventures in Wonderland")
+                Text("Lewis Carroll")
+                Spacer(modifier = Modifier.height(20.dp))
+                Text("32%")
+            }
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                var showSheet by remember { mutableStateOf(false) }
+
+//            if (showSheet) {
+//                BottomSheet() {
+//                    showSheet = false
+//                }
+//            }
+
+                IconButton(onClick = {
+
+                }) {
+                    Icon(
+                        imageVector = Icons.Rounded.CheckCircle,
+                        contentDescription = "uploaded",
+                        modifier = Modifier
+                            .width(20.dp)
+                            .height(20.dp)
+                    )
+                }
+                IconButton(onClick = { showSheet = true }) {
+                    Icon(
+                        imageVector = Icons.Sharp.MoreVert,
+                        contentDescription = "more",
+                        modifier = Modifier
+                            .width(20.dp)
+                            .height(20.dp)
+                    )
+                }
+            }
+        }
+    }
+
+
 }
--- frontend/app/src/main/java/com/example/shareader/EbookView.kt
+++ frontend/app/src/main/java/com/example/shareader/EbookView.kt
@@ -1,17 +1,34 @@
 package com.example.shareader
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
@@ -34,6 +51,7 @@ fun EbookView() {
             modifier = Modifier.padding(15.dp),
             verticalArrangement = Arrangement.SpaceBetween
         ) {
+            TopBar(name = "")
             Text("Alice was beginning to get very tired of sitting by her sister on the bank, " +
                     "and of having nothing to do: once or twice she had peeped into the book her " +
                     "sister was reading, but it had no pictures or conversations in it, 'and what is the " +
@@ -42,8 +60,8 @@ fun EbookView() {
                     " made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain" +
                     " would be worth the trouble of getting up and picking the daisies, when suddenly" +
                     " a White Rabbit with pink eyes ran close by her.",
-                fontSize = 25.sp,
-                lineHeight = 28.sp,
+                fontSize = 20.sp,
+                lineHeight = 23.sp,
                 textAlign = TextAlign.Start
             )
             Spacer(modifier = Modifier.height(10.dp))
@@ -65,3 +83,39 @@ fun EbookView() {
 //    ) {
 //    }
 //}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(name: String) {
+    val context = LocalContext.current
+    var expanded by remember { mutableStateOf(false) }
+    val activityLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { _ ->
+    }
+    TopAppBar(
+        title = { Text(text = name) },
+        navigationIcon = {
+            IconButton(onClick = {
+                val intent = Intent(context, MainActivity::class.java)
+                activityLauncher.launch(intent)
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Arrow Back"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = {expanded = !expanded}) {
+                Icon(
+                    imageVector = Icons.Filled.MoreVert,
+                    contentDescription = "Menu",
+                    tint = Color.Black
+                )
+            }
+        }
+    )
+
+}
