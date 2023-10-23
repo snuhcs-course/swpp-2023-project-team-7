a3afd8914cf7b4a9163d554acba9102a3e325911
Ikjun Choi <1350adwx@snu.ac.kr>
2023-10-23 20:48:11 +0900

    feat: Implement Add Book Page, Integrate with API

--- frontend/app/src/main/java/com/example/shareader/BookListView.kt
+++ frontend/app/src/main/java/com/example/shareader/BookListView.kt
@@ -1,11 +1,11 @@
 package com.example.shareader
 
+import BottomSheet
 import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,23 +13,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.CheckCircle
-import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.sharp.MoreVert
-import androidx.compose.material.icons.sharp.Settings
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,22 +27,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.material3.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import BottomSheet
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun BookListView() {
-    Scaffold (
-        floatingActionButton = { AddButton() }
-    ){
+    Scaffold(floatingActionButton = { AddButton() }) {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background,
@@ -89,8 +75,16 @@ fun BookListView() {
 
 @Composable
 fun AddButton() {
+    val context = LocalContext.current
+    val activityLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { _ ->
+    }
     FloatingActionButton(
-        onClick = {  },
+        onClick = {
+            val intent = Intent(context, AddBookActivity::class.java)
+            activityLauncher.launch(intent)
+        },
     ) {
         Icon(Icons.Filled.Add, "Floating action button.")
     }
@@ -105,13 +99,15 @@ fun BookCard() {
     ) { _ ->
     }
     Card(
-        onClick = { val intent = Intent(context, ReaderActivity::class.java)
-            activityLauncher.launch(intent) },
+        onClick = {
+            val intent = Intent(context, ReaderActivity::class.java)
+            activityLauncher.launch(intent)
+        },
         colors = CardDefaults.cardColors(
             containerColor = Color.Transparent
         ),
     ) {
-        Row (
+        Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
---
+++ frontend/app/src/main/java/com/example/shareader/ui/viewmodels/AddBookViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.shareader.ui.viewmodels
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import com.example.shareader.ui.models.AddBookModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class AddBookViewModel : ViewModel() {
+    private var coverUrl = ""
+    private var scope = CoroutineScope(Dispatchers.IO)
+
+    var snackbarMessage = MutableStateFlow("")
+    var bookUploaded = MutableStateFlow(false)
+
+    fun uploadImage(image: Bitmap) {
+        scope.launch {
+            AddBookModel.getInstance().uploadImage(image).onSuccess {
+                coverUrl = it
+                snackbarMessage.value = "Image uploaded successfully"
+            }.onFailure {
+                println(it)
+                snackbarMessage.value = "Image upload failed"
+            }
+        }
+    }
+
+    fun addBook(title: String, author: String, content: String) {
+        scope.launch {
+            AddBookModel.getInstance().addBook(title, author, content, coverUrl).onSuccess {
+                println("success")
+                snackbarMessage.value = "Book added successfully"
+                bookUploaded.value = true
+            }.onFailure {
+                println(it)
+                snackbarMessage.value = "Book add failed"
+            }
+        }
+    }
+
+    fun clearSnackbar() {
+        snackbarMessage.value = ""
+    }
+}
