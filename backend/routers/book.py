5a37b0b0d84b7997a27c34eb044a2109d5b26c47
Minjoo Jeon <wnals0619@snu.ac.kr>
2023-10-28 15:12:05 +0900

    feat: add users db, implement book list apis

--- backend/routers/book.py
+++ backend/routers/book.py
@@ -8,84 +8,110 @@ import os
 import uuid
 
 class BookAddRequest(BaseModel):
-  email: str
-  title: str
-  content: str
-  author: str = None
-  cover_image: str = None
-  
+    # TODO: Replace email when using OAuth
+    email: str
+    title: str
+    content: str
+    author: str = None
+    cover_image: str = None
+
 books_db = mysql.connector.connect(
-	host=os.environ["MYSQL_ENDPOINT"],
-	user=os.environ["MYSQL_USER"],
-	password=os.environ["MYSQL_PWD"],
-	database="readability",
+    host=os.environ["MYSQL_ENDPOINT"],
+    user=os.environ["MYSQL_USER"],
+    password=os.environ["MYSQL_PWD"],
+    database="readability",
 )
 book = APIRouter()
 
 @book.get("/test_db")
 def test_book_get():
-	cursor = books_db.cursor()
-	cursor.execute(f"SELECT * FROM Books")
-	result = cursor.fetchall()
-	return {"test":result}
+    cursor = books_db.cursor()
+    cursor.execute(f"SELECT * FROM Books")
+    result = cursor.fetchall()
+    return {"test":result}
 
 @book.get("/books")
-def book_list():
-  return [
-      {"title": "book_title", "author": "book_author", "cover_image": "https://picsum.photos/200/300"},
-      {"title": "book_title", "author": "book_author", "cover_image": "https://picsum.photos/200/300"},
-      {"title": "book_title", "author": "book_author", "cover_image": "https://picsum.photos/200/300"},
-      {"title": "book_title", "author": "book_author", "cover_image": "https://picsum.photos/200/300"},
-      {"title": "book_title", "author": "book_author", "cover_image": "https://picsum.photos/200/300"}
-    ]
+def book_list(email: str):
+    cursor = books_db.cursor()
+    cursor.execute(f"SELECT * FROM Books WHERE email = '{email}'")
+    result = cursor.fetchall()
 
-@book.get("/book/{book_id}")
+    books = []
+    for row in result:
+        books.append({
+            "book_id": row[0],
+            "title": row[2],
+            "author": row[3],
+            "progress": row[4],
+            "cover_image": row[5],
+        })
+    return {"books": books}
+
+
+@book.get("/book/{book_id}/detail")
 def book_detail(book_id: str):
-  return {
-    "title": "book_title",
-    "author": "book_author",
-    "cover_image": "https://picsum.photos/200/300",
-    "content": "book_content"
-  }
+    cursor = books_db.cursor()
+    cursor.execute(f"SELECT * FROM Books WHERE id = '{book_id}'")
+    result = cursor.fetchall()
 
+    return {
+        "title": result[0][2],
+        "author": result[0][3],
+        "progress": result[0][4],
+        "cover_image": result[0][5],
+    }
 
-@book.post("/book/{book_id}/progress")
+@book.put("/book/{book_id}/progress")
 def book_progress(book_id: str, progress: float):
-  return {}
+    cursor = books_db.cursor()
+    cursor.execute(f"UPDATE Books SET progress = {progress} WHERE id = '{book_id}'")
+    books_db.commit()
+    return {}
 
 @book.post("/book/add")
 def book_add(req: BookAddRequest):
-  user_dirname = req.email.replace(".", "")
-  user_dirname = f"/home/swpp/readability_users/{user_dirname}"
+    # from the Users table, get the user's username by querying with the email
+    cursor = books_db.cursor()
+    cursor.execute(f"SELECT username FROM Users WHERE email = '{req.email}'")
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return {"error": "User does not exist"}
 
-  assert os.path.isdir(user_dirname) == True
-  book_uuid = uuid.uuid4()
-  image_url = f"{user_dirname}/{book_uuid}.png"
-  content_url = f"{user_dirname}/{book_uuid}.txt"
+    # get username from result
+    username = result[0][0]
+    user_dirname = f"/home/swpp/readability_users/{username}"
+    assert os.path.isdir(user_dirname) is True
 
-  with open(content_url, 'w') as book_file:
-    book_file.write(req.content)
+    book_uuid = uuid.uuid4()
+    image_url = f"{user_dirname}/{book_uuid}.png"
+    content_url = f"{user_dirname}/{book_uuid}.txt"
 
-  image = Image.open(io.BytesIO(req.cover_image))
-  image.save(image_url)
+    with open(content_url, 'w') as book_file:
+        book_file.write(req.content)
+    # asssumes that the client is sending the image as a byte array.
+    # TODO: decide encoding of the image
+    image = Image.open(io.BytesIO(req.cover_image))
+    image.save(image_url)
 
-  add_book = (
-    "INSERT INTO Books (email, title, author, progress, cover_image, content)"
-    "VALUES (%s, %s, %s, %s, %s, %s)"
-  )
-  test_data = (req.email, req.title, req.author, 0.0, image_url, content_url)
+    add_book = (
+        "INSERT INTO Books (email, title, author, progress, cover_image, content)"
+        "VALUES (%s, %s, %s, %s, %s, %s)"
+    )
+    image_url = "/".join(image_url.split("/")[-2:])
+    content_url = "/".join(content_url.split("/")[-2:])
+    test_data = (req.email, req.title, req.author, 0.0, image_url, content_url)
 
-  cursor = books_db.cursor()
-  cursor.execute(add_book, test_data)
-  books_db.commit()
-  return {}
+    cursor = books_db.cursor()
+    cursor.execute(add_book, test_data)
+    books_db.commit()
+    return {}
 
-@book.post("/book/image/upload")
-def book_image_upload(file: UploadFile = File(...)):
-  # TODO: return url of uploaded image
-  return { "url": "https://picsum.photos/200/300" }
+@book.get("/book/image")
+def book_image(image_url: str):
+    image_url = os.path.join('/home/swpp/readability_users', image_url)
+    return FileResponse(image_url)
 
-@book.get("/book/image/{image_id}")
-def book_image(image_id: str):
-  # TODO: create database to store image_id - image_path mapping
-  return FileResponse("image.jpg")
+@book.get("/book/content")
+def book_content(content_url: str):
+    content_url = os.path.join('/home/swpp/readability_users', content_url)
+    return FileResponse(content_url)
